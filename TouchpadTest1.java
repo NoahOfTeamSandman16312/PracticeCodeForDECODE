e5b2804f4fe9882f7ecf0a65e4e58f1ccbdbcead
Noah <kidclark27@gmail.com>
2025-10-10 17:15:08 -0400

    It is officially an opmode

--- TouchpadTest1.java
+++ TouchpadTest1.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
  @TeleOp(name="TouchyTesty1", group="Linear Opmode")
- @Disabled
+ 
 
 public class TouchpadDriverTest1 {
     public static void main() {
