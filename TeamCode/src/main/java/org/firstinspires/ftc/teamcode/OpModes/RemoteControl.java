3758954d80fbc349809697c3af088b22a28f72cb
Noah <kidclark27@gmail.com>
2025-10-10 17:13:31 -0400

    Add RemoteControl.java cloned from ChristopherRoussakies/DecodeFtcRobotController

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RemoteControl.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.PoseStorage;
+
+@TeleOp
+public class RemoteControl extends OpMode {
+
+    MecanumDrive drive;
+    @Override
+    public void init(){
+        drive = new MecanumDrive(hardwareMap, PoseStorage.currentPose);
+    }
+
+    @Override
+    public void loop(){
+        drive.setDrivePowers(new PoseVelocity2d(
+                new Vector2d(
+                        -gamepad1.left_stick_y,
+                        -gamepad1.left_stick_x
+                ),
+                -gamepad1.right_stick_x
+        ));
+
+        drive.updatePoseEstimate();
+
+        Pose2d pose = drive.localizer.getPose();
+        telemetry.addData("x", pose.position.x);
+        telemetry.addData("y", pose.position.y);
+        telemetry.addData("heading (deg)", Math.toDegrees(pose.heading.toDouble()));
+        telemetry.update();
+    }
+
+}
