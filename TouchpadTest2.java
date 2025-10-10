74c36280110014e797318f3bc1ebd96f4e827e50
Noah <kidclark27@gmail.com>
2025-10-10 17:13:51 -0400

    Update TouchpadTest2.java

--- TouchpadTest2.java
+++ TouchpadTest2.java
@@ -1,1 +1,18 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+ @TeleOp(name="TouchyTesty1", group="Linear Opmode")
+ @Disabled
+
+public class TouchpadDriverTest1 {
+    public static void main() {
+    if(gamepad.1_touchpad_finger_1){
+        telemetry.adddata("You put 1 finger on the touchpad at this location /n x:"+gamepad.1_touchpad_finger_1_x+"/n y:"+gamepad.1_touchpad_finger_1_y+"/n Thank you for testing!");
+        telemetry.update();
+      
+    }
+    }
+}
