76942431b8906c0ca3069484b54e75f83c6e397f
Noah <kidclark27@gmail.com>
2025-10-16 15:10:50 -0400

    Correct gamepad variable references for touchpad input

--- TouchpadTest2.java
+++ TouchpadTest2.java
@@ -18,9 +18,9 @@ import org.firstinspires.ftc.teamcode.TestServo;
 
 public class TouchpadDriverTest1 {
     public static void main() {
-    if(gamepad.1_touchpad_finger_1){
+    if(gamepad1_touchpad_finger_1){
         telemetry.adddata("You put 1 finger on the touchpad at this location /n x:"+gamepad.1_touchpad_finger_1_x+"/n y:"+gamepad.1_touchpad_finger_1_y+"/n Thank you for testing!");
-				}if else(gamepad.1_touchpad_finger_2){
+				}if else(gamepad1_touchpad_finger_2){
 					 telemetry.adddata("You put 2 fingers on the touchpad at this location /n x:"+gamepad.1_touchpad_finger_2_x+"/n y:"+gamepad.1_touchpad_finger_2_y+"/n Thank you for testing!");
     }else{
 					telemetry.adddata("You have put no fingers on the touchpad");
