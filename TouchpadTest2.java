d5e0e7ac2b900aff5093a60bb9b5d92082a95355
Noah <kidclark27@gmail.com>
2025-10-16 15:30:15 -0400

    Fix touchpad finger detection in TouchpadDriverTest2

--- TouchpadTest2.java
+++ TouchpadTest2.java
@@ -16,12 +16,13 @@ import org.firstinspires.ftc.teamcode.TestServo;
  @TeleOp(name="TouchyTesty1", group="Linear Opmode")
  @Disabled
 
+	 
 public class TouchpadDriverTest2 {
     public static void main() {
-    if(gamepad1_touchpad_finger_1){
+    if(gamepad1.touchpad_finger_1){
         telemetry.adddata("You put 1 finger on the touchpad at this location /n x:"+gamepad1.touchpad_finger_1_x+"/n y:"+gamepad1.touchpad_finger_1_y+"/n Thank you for testing!");
-				}if else(gamepad1_touchpad_finger_2){
-					 telemetry.adddata("You put 2 fingers on the touchpad at this location /n x:"+gamepad1.touchpad_finger_2_x+"/n y:"+gamepad1_touchpad_finger_2_y+"/n Thank you for testing!");
+				}if else(gamepad1.touchpad_finger_2){
+					 telemetry.adddata("You put 2 fingers on the touchpad at this location /n x:"+gamepad1.touchpad_finger_2_x+"/n y:"+gamepad1.touchpad_finger_2_y+"/n Thank you for testing!");
     }else{
 					telemetry.adddata("You have put no fingers on the touchpad");
 				}
