4002bf3999c04b74dc4959f96934bf9c10a8b2aa
Noah <kidclark27@gmail.com>
2025-10-16 15:29:02 -0400

    Rename class and fix touchpad data references

--- TouchpadTest2.java
+++ TouchpadTest2.java
@@ -9,21 +9,22 @@ import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.TestServo;
  @TeleOp(name="TouchyTesty1", group="Linear Opmode")
  @Disabled
 
-public class TouchpadDriverTest1 {
+public class TouchpadDriverTest2 {
     public static void main() {
     if(gamepad1_touchpad_finger_1){
-        telemetry.adddata("You put 1 finger on the touchpad at this location /n x:"+gamepad.1_touchpad_finger_1_x+"/n y:"+gamepad.1_touchpad_finger_1_y+"/n Thank you for testing!");
+        telemetry.adddata("You put 1 finger on the touchpad at this location /n x:"+gamepad1.touchpad_finger_1_x+"/n y:"+gamepad1.touchpad_finger_1_y+"/n Thank you for testing!");
 				}if else(gamepad1_touchpad_finger_2){
-					 telemetry.adddata("You put 2 fingers on the touchpad at this location /n x:"+gamepad.1_touchpad_finger_2_x+"/n y:"+gamepad.1_touchpad_finger_2_y+"/n Thank you for testing!");
+					 telemetry.adddata("You put 2 fingers on the touchpad at this location /n x:"+gamepad1.touchpad_finger_2_x+"/n y:"+gamepad1_touchpad_finger_2_y+"/n Thank you for testing!");
     }else{
 					telemetry.adddata("You have put no fingers on the touchpad");
 				}
 					telemetry.update();
+		
 }
