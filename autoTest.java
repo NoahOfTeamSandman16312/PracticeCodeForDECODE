cdf6cc5f03ba53a0e54547c4bd2047f2a353ca02
Noah <kidclark27@gmail.com>
2025-10-16 15:15:59 -0400

    Add package and import statements to autoTest.java

--- autoTest.java
+++ autoTest.java
@@ -1,1 +1,15 @@
+package org.firstinspires.ftc.teamcode.OpModes;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.PoseStorage;
+
+@Auto
