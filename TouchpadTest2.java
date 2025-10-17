0bfdbd316c58b3517b3dd9537748cd1542ba98d8
Noah <kidclark27@gmail.com>
2025-10-17 10:52:24 -0400

    Rename TeleOp and fix telemetry messages

--- TouchpadTest2.java
+++ TouchpadTest2.java
@@ -13,16 +13,15 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.TestServo;
- @TeleOp(name="TouchyTesty1", group="Linear Opmode")
- @Disabled
+ @TeleOp(name="TouchyTesty2", group="Linear Opmode")
 
 	 
 public class TouchpadDriverTest2 {
     public static void main() {
     if(gamepad1.touchpad_finger_1){
-        telemetry.adddata("You put 1 finger on the touchpad at this location /n x:"+gamepad1.touchpad_finger_1_x+"/n y:"+gamepad1.touchpad_finger_1_y+"/n Thank you for testing!");
+        telemetry.adddata("You put 1 finger on the touchpad at this location \n x:"+gamepad1.touchpad_finger_1_x+"\n y:"+gamepad1.touchpad_finger_1_y+"\n Thank you for testing!\n");
 				}if else(gamepad1.touchpad_finger_2){
-					 telemetry.adddata("You put 2 fingers on the touchpad at this location /n x:"+gamepad1.touchpad_finger_2_x+"/n y:"+gamepad1.touchpad_finger_2_y+"/n Thank you for testing!");
+					 telemetry.adddata("You put 2 fingers on the touchpad at this location \n x:"+gamepad1.touchpad_finger_2_x+"\n y:"+gamepad1.touchpad_finger_2_y+"\n Thank you for testing!");
     }else{
 					telemetry.adddata("You have put no fingers on the touchpad");
 				}
